4d57fdf219b4779188770908f2fe0f71872210f1
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-09-02 02:57:15 +0530

    Nothing chenage

--- src/main/java/com/bl/java/functional_interface/NumberPlayList.java
+++ src/main/java/com/bl/java/functional_interface/NumberPlayList.java
@@ -1,4 +1,50 @@
 package com.bl.java.functional_interface;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class NumberPlayList {
+    public static void main(String[] args) {
+
+        List<Integer> myNumberList = new ArrayList<>();
+        for (int i=0; i<=5;i++) myNumberList.add(i);
+
+        //Method 1 : Traversing using Iterator
+        Iterator<Integer> it = myNumberList.iterator();
+        while (it.hasNext()){
+            Integer i = it.next();
+            System.out.println("Iterator Value: "+i);
+        }
+
+        //Method 2 : Traversing with Consumer interface Implementation
+        class MyConsumer implements Consumer<Integer>{
+            @Override
+            public void accept(Integer t) {
+                System.out.println("Mth2: Consumer impl Value : "+t);
+            }
+        }
+
+        MyConsumer action = new MyConsumer();
+        myNumberList.forEach(action);
+
+        //Method 3: Traversing with Anonymous Consumer interface implementation
+        myNumberList.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer t) {
+                System.out.println("Mth3: forEach Anonymous class value : "+t);
+            }
+        });
+
+        // Method 4: Explicit Lambda Function
+        Consumer<Integer> myListAction = n ->{
+            System.out.println("Mth4 : forEach Lambda impl Value : "+ n);
+        };
+        myNumberList.forEach(myListAction);
+        //Method 5 : Implicit Lambda Function
+        myNumberList.forEach(n->{
+            System.out.println("Mth5 : forEach Lambda impl Value : "+n);
+        });
+    }
 }
