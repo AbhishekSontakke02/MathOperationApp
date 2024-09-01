4c44cf44df9b1f20e8f6a659c805b103146ae14b
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-09-02 00:44:14 +0530

    Implemented : UC1.1 calcaulte as functionalInterface abstaract method and arethmatic opration functions using Lambda Expression

--- src/main/java/com/bl/java/functional_interface/MathOpertations.java
+++ src/main/java/com/bl/java/functional_interface/MathOpertations.java
@@ -1,4 +1,20 @@
 package com.bl.java.functional_interface;
 
+@FunctionalInterface
+interface IMathFunction{
+    int calculate(int a, int b);
+}
+
 public class MathOpertations {
+    public static void main(String[] args) {
+        IMathFunction add = Integer::sum;
+        IMathFunction sub = (a,b) -> a-b;
+        IMathFunction mul = (a,b) -> a*b;
+        IMathFunction div = (int a, int b) -> a/b;
+
+        System.out.println("Addition is " + add.calculate(6,3));
+        System.out.println("Subtraction is " + sub.calculate(6,3));
+        System.out.println("Multiplication is " + mul.calculate(6,3));
+        System.out.println("Division is " + div.calculate(6,3));
+    }
 }
