16d94c3b41b88bfcd1edb5c19f979b7c5a0dca2a
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-09-02 00:54:19 +0530

    Implemented: printResult() to pass lambda expression and show reesult

--- src/main/java/com/bl/java/functional_interface/MathOpertations.java
+++ src/main/java/com/bl/java/functional_interface/MathOpertations.java
@@ -3,6 +3,9 @@ package com.bl.java.functional_interface;
 @FunctionalInterface
 interface IMathFunction{
     int calculate(int a, int b);
+    static void printResult(int a, int b, String function, IMathFunction fobj){
+        System.out.println("Result of "+function+" is "+ fobj.calculate(a,b));
+    }
 }
 
 public class MathOpertations {
@@ -12,9 +15,14 @@ public class MathOpertations {
         IMathFunction mul = (a,b) -> a*b;
         IMathFunction div = (int a, int b) -> a/b;
 
-        System.out.println("Addition is " + add.calculate(6,3));
-        System.out.println("Subtraction is " + sub.calculate(6,3));
-        System.out.println("Multiplication is " + mul.calculate(6,3));
-        System.out.println("Division is " + div.calculate(6,3));
+//        System.out.println("Addition is " + add.calculate(6,3));
+//        System.out.println("Subtraction is " + sub.calculate(6,3));
+//        System.out.println("Multiplication is " + mul.calculate(6,3));
+//        System.out.println("Division is " + div.calculate(6,3));
+
+        IMathFunction.printResult(6,3,"Addition",add);
+        IMathFunction.printResult(6,3,"Subtraction",sub);
+        IMathFunction.printResult(6,3,"Multiplication",mul);
+        IMathFunction.printResult(6,3,"Division",div);
     }
 }
